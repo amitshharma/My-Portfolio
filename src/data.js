7f7696b09350dc5a806948c0c547a35135a6021b
Amit Sharma <99009356+amitshharma@users.noreply.github.com>
2022-07-13 23:27:35 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,33 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/GitHub_logo_2013.svg/220px-GitHub_logo_2013.svg.png",
+      link: "https://github.com/amitshharma",
+    },
+    {
+      id: 2,
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/200px-LinkedIn_Logo.svg.png",
+      link: "www.linkedin.com/in/amitshharma",
+    },
+    {
+      id: 3,
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/HackerRank_Icon-1000px.png/220px-HackerRank_Icon-1000px.png",
+      link: "https://www.hackerrank.com/sharmmaamit",
+    },
+    {
+      id: 4,
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/220px-Twitter-logo.svg.png",
+      link: "https://twitter.com/amit_shharma",
+    },
+    {
+      id: 5,
+      img: "https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Facebook_f_logo_%282021%29.svg/150px-Facebook_f_logo_%282021%29.svg.png",
+      link: "https://www.facebook.com/sharma.amit01",
+    },
+    {
+      id: 6,
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/150px-Instagram_logo_2022.svg.png",
+      link: "https://www.instagram.com/amit.shharma",
+    },
+  ];
+  
