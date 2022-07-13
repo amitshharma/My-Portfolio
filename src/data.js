55d17afbe904cb16309301a099429aa673119d30
Amit Sharma <99009356+amitshharma@users.noreply.github.com>
2022-07-14 01:04:49 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,32 +1,41 @@
+
+import inst from "./img/insta.png"
+import git from "./img/git.png"
+import hack from "./img/hack.png"
+import tweet from "./img/twitter.png"
+import link from "./img/link.png"
+import fb from "./img/fb.png"
+
+
 export const products = [
     {
       id: 1,
-      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/GitHub_logo_2013.svg/220px-GitHub_logo_2013.svg.png",
+      img:git,
       link: "https://github.com/amitshharma",
     },
     {
       id: 2,
-      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/200px-LinkedIn_Logo.svg.png",
+      img:link ,
       link: "www.linkedin.com/in/amitshharma",
     },
     {
       id: 3,
-      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/HackerRank_Icon-1000px.png/220px-HackerRank_Icon-1000px.png",
+      img: hack,
       link: "https://www.hackerrank.com/sharmmaamit",
     },
     {
       id: 4,
-      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/220px-Twitter-logo.svg.png",
+      img:tweet ,
       link: "https://twitter.com/amit_shharma",
     },
     {
       id: 5,
-      img: "https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Facebook_f_logo_%282021%29.svg/150px-Facebook_f_logo_%282021%29.svg.png",
+      img:fb ,
       link: "https://www.facebook.com/sharma.amit01",
     },
     {
       id: 6,
-      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/150px-Instagram_logo_2022.svg.png",
+      img: inst,
       link: "https://www.instagram.com/amit.shharma",
     },
   ];
